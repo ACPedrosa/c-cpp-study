ea22e005e97483db027cd4221afbf698746c16eb
Ana Caroline <155763806+ACPedrosa@users.noreply.github.com>
2025-04-19 23:39:10 -0300

    Update and rename hello-world.c to helloWorld.c
    hello world program

--- C/helloWorld/hello-world.c
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-unsigned namespace std;
-
-main(){
-
-}
-
---
+++ C/helloWorld/helloWorld.c
@@ -0,0 +1,14 @@
+/*
+    Programa: helloWolrd.c
+    Autor(a): Ana Caroline Pedrosa
+    Objetivo: imprimir um "hello world"
+*/
+#include <iostream>
+
+unsigned namespace std;
+
+int main(){
+  cout << "Hello World" << endl;
+  return 0;
+}
+
